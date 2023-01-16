17fa68fa1c1e8a57ad52fd4ae88b476d9b0c9e5e
Kirill Che <kot313@gmail.com>
2023-01-17 01:55:20 +0300

    1

--- main.go
+++ main.go
@@ -3,10 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hi")
-	fmt.Println("hi2")
-	fmt.Println("hi3")
-	fmt.Println("hi4")
-	fmt.Println("hi5")
-	fmt.Println("hi8")
+	fmt.Println(1)
 }
